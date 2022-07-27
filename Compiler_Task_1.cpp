0a84614bfa7eacfa58a2e05d192ece19e0e8c745
AJ-ROY <aj.anik1305@gmail.com>
2022-07-28 01:07:42 +0600

    Uploading from Ajoy

--- Compiler_Task_1.cpp
+++ Compiler_Task_1.cpp
@@ -64,6 +64,55 @@ else if(ex[1]=='*'){
     }
 
 
+///From here is Ajoy Roy part
+
+    else if(esz==3 and ex[2]=='*' and ex[1]!='U' and ex[1]!='+' and ex[1]!='*'){
+        cout<<"Enter a string to verify [$ for empty string]: ";
+        cin>>str;
+        int sz=str.size();
+
+        if(str[0]!=ex[0])f=0;
+        if(f){
+            for(int i=1;i<sz;i++){
+                if(str[i]!=ex[1]){
+                    f=0;
+                    break;
+                }
+            }
+        }
+        if(f)cout<<"status: valid"<<endl;
+        else cout<<"status: invalid"<<endl;
+    }
+    else if(ex[1]=='+'){
+
+        if(esz==2){
+            cout<<"Enter a string to verify [$ for empty string]: ";
+            cin>>str;
+            int sz=str.size();
+            for(int i=0;i<sz;i++){
+                if(str[i]!=ex[0]){
+                    f=0;
+                    break;
+                }
+            }
+            if(f)cout<<"status: valid"<<endl;
+            else cout<<"status: invalid"<<endl;
+        }
+        else if(esz>2 and ex[2]!='*' and ex[2]!='U' and ex[2]!='+'){
+            cout<<"Enter a string to verify [$ for empty string]: ";
+            cin>>str;
+            int sz=str.size();
+            for(int i=0;i<sz-1;i++){
+                if(str[i]!=ex[0]){
+                    f=0;
+                    break;
+                }
+            }
+            if(f and str[sz-1]==ex[2])cout<<"status: valid"<<endl;
+            else cout<<"status: invalid"<<endl;
+        }
+        else cout<<"status: invalid expression"<<endl;
+    }
 
 
 /// This is the Main part
